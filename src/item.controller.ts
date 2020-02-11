335402d9e152793a5ba0b4854a96dac8c5414d20
Mohamed EL-FEKY <mohamed.elfeky@alten.se>
2020-02-11 05:03:10 +0100

    exercise 2 solution

--- src/item.controller.ts
+++ src/item.controller.ts
@@ -1,29 +1,62 @@
 import * as express from 'express';
+import { ItemModel } from './item';
 
 const itemRoutes = express.Router();
 itemRoutes.get('/item', async (req: express.Request, resp: express.Response, next: express.NextFunction) => {
-	console.log("getting Items");
+	try {
+		let items: any = await ItemModel.find({});
+		resp.json(items);
+	} catch (err) {
+		resp.status(500);
+		resp.end();
+		console.error('Caught error', err);
+	}
+ });
+ 
+ itemRoutes.post('/item', async (req: express.Request, resp: express.Response, next: express.NextFunction) => {
+	const itemJson = req.body;
+	const item = new ItemModel(itemJson);
+	await item.save();
 	resp.end();
-});
-
-itemRoutes.post('/item', async (req: express.Request, resp: express.Response, next: express.NextFunction) => {
-	console.log("saving item");
+ });
+ 
+ itemRoutes.put('/item/:id', async (req: express.Request, resp: express.Response, next: express.NextFunction) => {
+	const id = req.params['id'];
+	let item;
+	try {
+		item = await ItemModel.findById(id);
+		if(item.status == null){
+			item.status = "SOLD";
+			await ItemModel.findOneAndUpdate({
+				_id: id
+			}, item, { upsert: true });
+			resp.status(200);
+		}else{
+			resp.status(409);
+		}
+	} catch (error) {
+		console.error(error);
+		resp.status(500);
+	}
 	resp.end();
-});
-
-itemRoutes.put('/item/:id', async (req: express.Request, resp: express.Response, next: express.NextFunction) => {
-	console.log("Editing Item");
+ });
+ 
+ itemRoutes.delete('/item/:id', async (req: express.Request, resp: express.Response, next: express.NextFunction) => {
+	const id = req.params['id'];
+	await ItemModel.findByIdAndRemove(id);
 	resp.end();
-});
-
-itemRoutes.delete('/item/:id', async (req: express.Request, resp: express.Response, next: express.NextFunction) => {
-	console.log("deleting item");
+ });
+ 
+ itemRoutes.get('/item/:id', async (req: express.Request, resp: express.Response, next: express.NextFunction) => {
+	const id = req.params['id'];
+	let item;
+	try {
+		item =await ItemModel.findById(id);
+		resp.json(item);
+	} catch (error) {
+		resp.status(404);
+	}
 	resp.end();
-});
-
-itemRoutes.get('/item/:id', async (req: express.Request, resp: express.Response, next: express.NextFunction) => {
-	console.log("getting item");
-	resp.end();
-});
+ });
 
 export { itemRoutes }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { app } from './app';
 import * as http from 'http';
+import * as mongoose from 'mongoose';
 
+const MONGO_URI = 'mongodb://localhost:27017/item';
 const port = 8080;
 const server = http.createServer(app);
 
@@ -8,6 +10,17 @@ server.listen(port);
 server.on('error', (err) => {
  console.error(err);
 });
-server.on('listening', () => {
+server.on('listening', async () => {
    console.info(`Listening on port ${port}`);
+   mongoose.connect(MONGO_URI, 
+       { 
+           useNewUrlParser: true,
+           useFindAndModify: false
+       });
+   mongoose.connection.once('open', () => {
+       console.info('Connected to Mongo via Mongoose');
+   });
+   mongoose.connection.on('error', (err) => {
+       console.error('Unable to connect to Mongo via Mongoose', err);
+   });
 });
